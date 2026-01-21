75ab4f58acfc4a21f7d96103e0908327760085dc
Abel <lewheabel16@gmail.com>
2026-01-21 16:07:48 +0100

    appointments

--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtisanController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::get("/getArtisans", [ArtisanController::class, 'getAll']);
